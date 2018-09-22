afae011595ab31a66df27c791476ba3340aef314
RWOverdijk <r.w.overdijk@gmail.com>
2018-09-22 21:09:35 +0200

    refactor(project): update stix

---
+++ src/Library/Gates.ts
@@ -0,0 +1,15 @@
+import { GateConfigInterface } from '.';
+import { gatesMiddleware } from '../middleware';
+import { Config, Event, LoggerService, ModuleInterface, ModuleManager, ResponseService, ServerService } from 'stix';
+
+export class Gates implements ModuleInterface {
+  public onBootstrap(event: Event<ModuleManager>): void {
+    const serviceManager = event.getTarget().getApplication().getServiceManager();
+
+    serviceManager.get(ServerService).useBefore('dispatch', gatesMiddleware(
+      serviceManager.get(Config).of<GateConfigInterface>('gates'),
+      serviceManager.get(ResponseService),
+      serviceManager.get(LoggerService),
+    ));
+  }
+}
